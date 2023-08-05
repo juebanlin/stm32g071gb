0d0eea5a49fce086529aa3d6e478ec4f4a63f81b
juebanlin <juebanlin@gmail.com>
2023-08-05 17:53:53 +0800

    2

---
+++ tests/success/button.rs
@@ -0,0 +1,86 @@
+#![no_std]
+#![no_main]
+#![feature(alloc_error_handler)]
+extern crate alloc;
+
+use alloc::format;
+use core::alloc::Layout;
+use core::fmt::Write;
+use core::panic::PanicInfo;
+
+use alloc_cortex_m::CortexMHeap;
+use cortex_m_rt::entry;
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
+use embedded_hal::Direction;
+use rtt_target::{rprintln, rtt_init_print};
+use ssd1306::{I2CDisplayInterface, prelude::*, Ssd1306};
+use stm32g0xx_hal::{cortex_m, stm32 as device};
+use stm32g0xx_hal::analog::dac::GeneratorConfig;
+use stm32g0xx_hal::i2c::I2c;
+use stm32g0xx_hal::prelude::*;
+use stm32g0xx_hal::rcc::Config;
+
+// this is the allocator the application will use
+#[global_allocator]
+static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+const HEAP_SIZE: usize = 512;
+
+//内存分配错误处理
+#[alloc_error_handler]
+fn oom(_: Layout) -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic_(_info:&PanicInfo) -> !{
+    loop {
+
+    }
+}
+
+#[entry]
+fn main() -> ! {
+    // 初始化内存分配器
+    // Initialize the allocator BEFORE you use it
+    {
+        use core::mem::MaybeUninit;
+        static mut HEAP: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+        unsafe { ALLOCATOR.init(HEAP.as_ptr() as usize, HEAP_SIZE) }
+    }
+    rtt_init_print!();
+    rprintln!("rtt init success!");
+    button_demo();
+}
+
+fn button_demo() -> !{
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = device::Peripherals::take().unwrap();
+    let mut rcc = dp.RCC.constrain();
+    let mut delay = cp.SYST.delay(&mut rcc);
+    let gpioa = dp.GPIOA.split(&mut rcc);
+    //上拉输入,引脚处于高电平 按键接地判定低电平即为按键按下
+    let button = gpioa.pa7.into_pull_up_input();
+    let mut buttonPress=false;
+    loop {
+        let mut wait;
+        match button.is_high() {
+            Ok(true) => {
+                wait=300.millis();
+                buttonPress=false;
+            }
+            Ok(false) => {
+                wait=100.millis();
+                buttonPress=true;
+            }
+            _ => unreachable!(),
+        };
+        wait=100.millis();
+        delay.delay(wait);
+        rprintln!("buttonPress:{}",buttonPress);
+    }
+}
